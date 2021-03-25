dc221c08fc7cb10ea6d0286813f24d6cb0ac818f
Tim Ritter <tr@next-motion.de>
2021-03-25 19:42:23 +0100

    initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.global.css';
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/"/> #component={}
+      </Switch>
+    </Router>
+  );
+}
